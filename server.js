55b1adf66ad9e9b9d0b2f6a49502ead24e19bce4
Pravin Mhaske <pravinmhaske817@gmail.com>
2024-03-02 15:28:48 +0530

    NodeJS server created successfully with MVC setup

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// rest object
+const app = express();
+
+// configure the dotenv
+dotenv.config();
+
+// middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
+app.get("/", (req, res) => {
+  res.send("The server is running");
+});
+
+// Get the PORT
+const PORT = process.env.PORT || 8080;
+
+// Listen server
+app.listen(PORT, () => {
+  console.log(
+    `The server is running in ${process.env.NODE_MODE} on port : ${PORT}`
+      .bgGreen.white
+  );
+});
